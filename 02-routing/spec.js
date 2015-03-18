4d6773b1bc96063bf32bd4c611f84c80eb7ff665
Travis-CI <zordius@yahoo-inc.com>
2015-03-18 02:34:39 +0000

    Auto push new examples from zordius/fluxex@5a35f9d74f32ebd5127f5bd61a0ac4bac4eb2005

--- 02-routing/spec.js
+++ 02-routing/spec.js
@@ -4,13 +4,23 @@ describe('React server side rendering', function () {
         expect(element.all(by.css('ul li')).count()).toEqual(5);
     });
 
-    it('title should be main', function () {
+    it('body title should be main', function () {
         browser.get('main');
         expect(element(by.css('div h1')).getInnerHtml()).toMatch(/Main/);
+    });
+
+    it('html title should be Main Page', function () {
+        expect(element(by.css('title')).getInnerHtml()).toBe('Main Page');
     });
 });
 
 describe('React client side binding', function () {
+    it('should handle document.title correctly', function () {
+        browser.driver.executeScript('return document.title').then(function (value) {
+            expect(value).toBe('Main Page');
+        });
+    });
+
     it('should handle button click', function () {
         browser.get('main');
         browser.driver.executeScript('return window.test=1');
@@ -21,8 +31,40 @@ describe('React client side binding', function () {
             });
         });
         expect(element(by.css('div span')).getInnerHtml()).toMatch(/12300/);
+    });
+
+    it('should block page loading and use history.pushState', function () {
         browser.driver.executeScript('return window.test').then(function (value) {
             expect(value).toBe(1);
+        });
+    });
+
+    it('should change title correctly after page changed', funciton () {
+        browser.driver.executeScript('return document.title').then(function (value) {
+            expect(value).toMatch(/123/);
+        });
+    });
+
+    it('should handle browser back button', function () {
+        browser.driver.executeScript('return window.test=2');
+        browser.navigate().back();
+        browser.wait(function () {
+            return browser.driver.getCurrentUrl().then(function (url) {
+                return url.match(/main/);
+            });
+        });
+        expect(element(by.css('div h1')).getInnerHtml()).toMatch(/Main/);
+    });
+
+    it('should not cause page changed when back', function () {
+        browser.driver.executeScript('return window.test').then(function (value) {
+            expect(value).toBe(2);
+        });
+    });
+
+    it('should change title correctly after page back', funciton () {
+        browser.driver.executeScript('return document.title').then(function (value) {
+            expect(value).toBe('Main Page');
         });
     });
 });
